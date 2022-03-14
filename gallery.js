84b0eb3c09c07a47583a69920b93f3adaac25198
enes4124 <enes.karakaya5@etu.univ-lorraine.fr>
2022-03-14 15:23:15 +0100

    ok

--- gallery.js
+++ gallery.js
@@ -4,5 +4,5 @@ export function load(){
     let tab;
 
     let lr = loadResource("https://webetu.iutnc.univ-lorraine.fr/www/canals5/phox/api/photos");
-    console.log(lr);
+    console.log(lr.then());
 }
