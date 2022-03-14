08607401bfbe7bcfe12179de65ea6b420c4abe29
enes4124 <enes.karakaya5@etu.univ-lorraine.fr>
2022-03-14 14:54:06 +0100

    ok

---
+++ gallery_ui.js
@@ -0,0 +1,14 @@
+function display_galerie(galerie){
+
+    let a = document.getElementById("gallery_container");
+    a.innerHTML = "";
+
+    galerie.forEach(e => {
+        a.innerHTML += `       
+        <div class="vignette" >
+        <img data-uri="${e.links.self.href}"
+          src="${e.photo.original.href}">
+      </div>
+      `
+    });
+}
