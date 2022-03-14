33694947a1c4a72adeaa97a2b196a51444f30097
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-03-14 15:50:54 +0100

    Update photobox.js

--- photobox.js
+++ photobox.js
@@ -5,3 +5,11 @@ document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
     load().then(display_galerie);
   })
+
+let a = document.getElementsByClassName("vignette");
+
+for(let i=0;i<a.length;i++){
+    a[i].onclick = () => {
+        console.log("it works");
+    };
+}
