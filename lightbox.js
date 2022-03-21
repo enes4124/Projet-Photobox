1b37ff7644ba2520fe7d82859d4f77c8c2998f54
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-03-21 15:10:51 +0100

    Update lightbox.js

--- lightbox.js
+++ lightbox.js
@@ -4,7 +4,6 @@ import {display_lightbox} from "./lightbox_ui.js";
 export function load(node){
 
     let a = node.getAttribute("data-uri");
-    //photos.links.self.href=a;
-    console.log(a);
+    
     loadResource("https://webetu.iutnc.univ-lorraine.fr/www/canals5/phox/api/photos/6").then(display_lightbox);
 }
