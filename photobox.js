85acab37e9e151050db4edefcaef6f29c0ca6b6c
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-03-21 15:27:05 +0100

    Update photobox.js

--- photobox.js
+++ photobox.js
@@ -2,6 +2,15 @@ import {loadR} from "./gallery.js";
 import {display_galerie} from "./gallery_ui.js";
 import {load} from "./lightbox.js";
 
+
+let cacher = document.getElementById("lightbox_close")
+cacher.onclick = function(){
+    console.log("ok c'est ici le cachement");
+    document.getElementById("lightbox_container").classList.remove("lightbox_container--visible");
+    document.getElementById("lightbox_container").classList.add("lightbox_container--hidden");
+}
+
+
 document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
     loadR().then(display_galerie);
