18922b8986b75cf5af8c324122519daf5bbd30c9
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-03-21 15:44:33 +0100

    ns bg

--- gallery_ui.js
+++ gallery_ui.js
@@ -1,18 +1,29 @@
+import {load} from "./lightbox.js";
+
 export function display_galerie(galerie){
 
-    let a = document.getElementById("gallery_container");
-    a.innerHTML = "";
+    let b = document.getElementById("gallery_container");
+    b.innerHTML = "";
 
    //let sheesh = JSON.parse(galerie);
 
     galerie.photos.forEach(e => {
         // console.log(e.photo.original.href);
         //console.log(e.links.self.href);
-        a.innerHTML += `
+        b.innerHTML += `
         <div class="vignette" >
         <img data-uri="${e.links.self.href}"
           src="https://webetu.iutnc.univ-lorraine.fr/${e.photo.original.href}">
       </div>
       `
     });
+
+    let a = document.getElementsByClassName("vignette");
+    console.log(a);
+    for(let i=0;i<a.length;i++) {
+        a[i].onclick = () => {
+            console.log(a[i].children[0]);
+            load(a[i].children[0]);
+        };
+    }
 }
--- photobox.js
+++ photobox.js
@@ -15,13 +15,6 @@ document.querySelector('#load_gallery')
   .addEventListener('click',  e => {
     loadR().then(display_galerie);
 
-      let a = document.getElementsByClassName("vignette");
 
-      for(let i=0;i<a.length;i++) {
-          a[i].onclick = () => {
-              console.log(a[i].children[0]);
-              load(a[i].children[0]);
-          };
-      }
-  })
+  });
 
